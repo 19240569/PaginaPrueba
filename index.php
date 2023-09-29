b33a336ad45646ecc3ce9fb66ef9d8164f98f275
Fernanda Briones <145827385+19240569@users.noreply.github.com>
2023-09-28 19:43:53 -0600

    index.php

--- index.php
+++ index.php
@@ -1,17 +1,85 @@
 <?php
 session_start();
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
     <title>Guanajuato</title>
+    <style>
+        .bottom-image {
+            display: block;
+            margin: 20px auto;
+        }
+        body {
+            font-family: 'Arial', sans-serif;
+            color: #fff;
+            background-color: #ff5733;
+            text-align: center;
+            padding: 50px;
+        }
+
+        h1 {
+            font-size: 3em;
+            margin-bottom: 20px;
+            text-shadow: 3px 3px 5px rgba(0, 0, 0, 0.3);
+        }
+
+        ul {
+            list-style-type: none;
+            padding: 0;
+        }
+
+        li {
+            display: inline-block;
+            margin: 0 10px;
+        }
+
+        a {
+            text-decoration: none;
+            color: #fff;
+            font-size: 1.5em;
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+        }
+
+        form {
+            margin-top: 50px;
+        }
+
+        input {
+            padding: 10px;
+            font-size: 1em;
+            border: none;
+            border-radius: 5px;
+            margin-bottom: 10px;
+            width: 200px;
+        }
+
+        input[type="submit"] {
+            background-color: #ff5733;
+            color: #fff;
+            cursor: pointer;
+            font-size: 1em;
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+        }
+
+        input[type="submit"]:hover {
+            background-color: #e14c2e;
+        }
+
+        a.logout-btn {
+            display: block;
+            text-align: center;
+            font-size: 1.5em;
+            margin-top: 20px;
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+        }
+    </style>
 </head>
 <body>
-
+<img src="imagen1.jpg" alt="Imagen 1" class="bottom-image">
 <?php
 if(isset($_SESSION["usuario"])) {
-    echo "<h1>Bienvenido, " . $_SESSION["usuario"] . "!</h1>";
+    echo "<h1>Bienvenido/a, " . $_SESSION["usuario"] . "!</h1>";
     echo "<ul>
             <li><a href='historia.php'>Historia</a></li>
             <li><a href='cultura.php'>Cultura</a></li>
@@ -29,5 +97,6 @@ if(isset($_SESSION["usuario"])) {
 }
 
 ?>
+
 </body>
 </html>
